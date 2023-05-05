cc6cbb93b5b1bb95a33fb2034abc6da8477ab9c4
N <naohiro.suda@gmail.com>
2023-05-05 15:43:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 import os
 import streamlit as st 
+import openai
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
